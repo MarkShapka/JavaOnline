f9f6d36b76661fcebfb9cd28a699f21597a93c11
Mark Shapka <mark.shapka1@gmail.com>
2020-01-06 14:16:20 +0200

    Lesson1 Task1

---
+++ src/Lesson_1/Task_1/Cat.java
@@ -0,0 +1,60 @@
+package Lesson_1.Task_1;
+
+public class Cat {
+    String name;
+    int age;
+    String color;
+    double weight;
+
+    public Cat() {
+    }
+
+    public Cat(String name, int age, String color, double weight) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", color='" + color + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
